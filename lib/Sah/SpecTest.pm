3178dc04873c6706cdfcc0b8c9762d3aeb495056
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-09-09 04:18:24 +0700

    Add Sah::SpecTest, a dummy module just so other dists can express depending on share/spectest

---
+++ lib/Sah/SpecTest.pm
@@ -0,0 +1,14 @@
+package Sah::SpecTest;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Just a dummy module so dists can depends on it
+
+=head1 DESCRIPTION
+
+This is just a dummy module so other Perl distributions can depends on the
+spectest (this dist's C<share/spectest/>).
